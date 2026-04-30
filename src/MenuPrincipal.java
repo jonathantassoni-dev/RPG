007eef87c7fe6a45671b62a1e17452ea3ec18b3e
jonathantassoni-dev <jonathan.tassoni@outlook.com>
2026-04-30 20:36:29 -0300

    concluído primeira parte da classe MenuPrincipal

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -5,25 +5,25 @@ public class MenuPrincipal {
 
     public void exibirMenuPrincipal() {
         Scanner scanner = new Scanner(System.in);
-
         int opcao;
-        do {
+
             System.out.println("\n------ MENU ------");
             System.out.println("1. Escolha sua classe:");
             System.out.println("0. Sair");
-
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     System.out.println("Acessar menu de classes");
                     break;
-                case 2:
+                case 0:
                     System.out.println("Você saiu do RPG!");
-
+                    break;
+                default:
+                    System.out.println("Valor Incorreto!");
+                    break;
             }
-
-        }while(opcao != 0);
+            scanner.close();
     }
 
 

a264a32cc788e9b222c1cf63d471d4893852db12
jonathantassoni-dev <jonathan.tassoni@outlook.com>
2026-04-30 21:29:31 -0300

    Ajustado voltar em Menu

--- src/MenuClasses.java
+++ src/MenuClasses.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class MenuClasses {
 
-
     public void exibirMenuClasses() {
         Scanner scanner = new Scanner(System.in);
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
         int opcao;
 
         System.out.println("\n------ CLASSES ------");
         System.out.println("1. Guerreiro");
         System.out.println("2. Mago");
         System.out.println("3. Paladino");
-        System.out.println("0. <Voltar>");
+        System.out.println("0. Voltar");
         opcao = scanner.nextInt();
 
         switch (opcao) {
@@ -25,7 +25,8 @@ public class MenuClasses {
                 System.out.println("Mostra Paladino");
                 break;
             case 0:
-                System.out.println("Volta o menu");
+                //Volta o menu
+                menuPrincipal.exibirMenuPrincipal();
                 break;
             default:
                 System.out.println("Valor Invalido!");
--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -5,17 +5,18 @@ public class MenuPrincipal {
 
     public void exibirMenuPrincipal() {
         Scanner scanner = new Scanner(System.in);
-
+        MenuClasses menuClasses = new MenuClasses();
         int opcao;
 
             System.out.println("\n------ MENU ------");
-            System.out.println("1. Escolha sua classe:");
-            System.out.println("0. <Sair>");
+            System.out.println("1. Escolha sua classe");
+            System.out.println("0. Sair");
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Acessar menu de classes");
+                    //Acessa o menu de classes
+                    menuClasses.exibirMenuClasses();
                     break;
                 case 0:
                     System.out.println("Você saiu do RPG!");
@@ -26,9 +27,5 @@ public class MenuPrincipal {
             }
             scanner.close();
     }
-
-
-
-
 
 }
